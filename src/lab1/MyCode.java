ababd9485de0d26998ed998624112b6b8f82952e
yuyzz <yuyna120@gmail.com>
2018-06-26 11:40:09 +0700

    ็็้Hello World

---
+++ src/lab1/MyCode.java
@@ -0,0 +1,23 @@
+package lab1;
+
+public class MyCode {
+
+    public static void main(String[] args) {
+
+
+    System.out.println("Hello World.");
+
+    System.out.println("name:romnalin chimnu");
+    System.out.println("id:021");
+    System.out.println("group:สารสนเทศ");
+    System.out.println("println:มทร.");
+
+
+
+
+    }//main
+
+
+
+
+}//class
